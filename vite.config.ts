e6e9e61123f59729d2d05d79cf929da2d8116205
santaletters <santascertifiedletter@gmail.com>
2025-11-22 15:58:05 -0500

    Remove alias config causing path resolution issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      './components': '/components',
-      './utils': '/utils',
-      './styles': '/styles'
-    }
-  },
   server: {
     port: 3000,
     open: true

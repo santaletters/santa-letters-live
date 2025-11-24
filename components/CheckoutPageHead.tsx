59eb5be296771d8ca0397bff555c73cc58ae4b6c
santaletters <santascertifiedletter@gmail.com>
2025-11-24 10:53:42 -0500

    Fix CheckoutPageHead syntax for JSX

--- components/CheckoutPageHead.tsx
+++ components/CheckoutPageHead.tsx
@@ -9,19 +9,16 @@ import { Helmet } from 'react-helmet-async';
 export function CheckoutPageHead() {
   return (
     <Helmet>
-<!-- Trackdesk tracker begin -->
-<script async src="//cdn.trackdesk.com/tracking.js"></script>
-<script>
-  (function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");
-
-  trackdesk("directwebinteractive", "conversion", {
-    "conversionType": "lead"
-  });
-</script>
-<!-- Trackdesk tracker end -->
-
-    
-      
+      {/* Trackdesk tracker begin */}
+      <script async src="//cdn.trackdesk.com/tracking.js"></script>
+      <script>{`
+        (function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");
+        
+        trackdesk("directwebinteractive", "conversion", {
+          "conversionType": "lead"
+        });
+      `}</script>
+      {/* Trackdesk tracker end */}
     </Helmet>
   );
 }
